49146b6c2b22cf2ee506798be6948d6befbc8db8
aisayo <aisayo@icloud.com>
2017-10-30 00:30:13 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
+    @recipe.ingredients << @recipe
     redirect_to recipe_path(@recipe)
   end
 
